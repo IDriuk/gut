1fd43f7c71b1ceaa20830e6cbd75e9c1bbec3b87
Ivan Driuk <veytarn@gmail.com>
2018-05-03 07:35:47 +0300

    add card layout layer

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,7 +7,12 @@ class Main extends Component {
       <div className="application-main ">
         <div>
           <div className="clearfix container-lg mt-4 px-3">
-            <div className="h-card col-3 float-left pr-3"></div>
+            <div className="h-card col-3 float-left pr-3">
+              <a className="u-photo d-block tooltipped tooltipped-s"></a>
+              <div className="py-3"></div>
+              <div className="mb-3 user-profile-bio"></div>
+              <ul className="border-gray-light border-top py-3 vcard-details"></ul>
+            </div>
             <div className="col-9 float-left pl-2"></div>
           </div>
         </div>
