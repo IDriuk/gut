6e259c148d567525bc189fc5a92d2c5049fdc6a9
Ivan Driuk <veytarn@gmail.com>
2018-05-09 13:12:34 +0300

    create repos list from items

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -62,7 +62,15 @@ class Main extends Component {
                     <h2 className="f4 mb-2 text-normal">Pinned repositories</h2>
                     <form>
                       <ol className="mb-4 pinned-repos-list">
-                        <li className="border border-gray-dark mb-3 p-3 pinned-repo-item public reorderable rounded-1 sortable-button-item source">
+                        {[
+                          {name: "gut", desc: "reverse engineering of github frontend", language: "JavaScript"},
+                          {name: "m_gut", desc: "reverse engineering mobile version of github frontend", language: "JavaScript"},
+                          {name: "avato", desc: "Avito with React.js", language: "JavaScript"},
+                          {name: "m_avato", desc: "Frontend of mobile avito with React.js", language: "JavaScript"},
+                          {name: "yalp", desc: "Yelp frontend with React.js", language: "JavaScript"},
+                          {name: "m_yalp", desc: "Frontend of mobile yelp with React.js", language: "JavaScript"},
+                        ].map(({name, desc, language}, index) =>
+                        <li key={index} className="border border-gray-dark mb-3 p-3 pinned-repo-item public reorderable rounded-1 sortable-button-item source">
                           <div className="pinned-repo-item-content">
                             <span className="d-block position-relative">
                               <span className="float-left pinned-repository-handle pr-2">
@@ -71,19 +79,20 @@ class Main extends Component {
                                 </svg>
                               </span>
                               <a className="text-bold">
-                                <span className="repo">gut</span>
+                                <span className="repo">{name}</span>
                               </a>
                             </span>
-                            <p className="pinned-repo-desc text-gray text-small d-block mt-2 mb-3">reverse engineering of github frontend</p>
+                            <p className="pinned-repo-desc text-gray text-small d-block mt-2 mb-3">{desc}</p>
                             <p className="mb-0 f6 text-gray">
                               <span
                                 className="repo-language-color pinned-repo-meta"
                                 style={{backgroundColor: "#f1e05a"}}
                               ></span>
-                              JavaScript
+                              {language}
                             </p>
                           </div>
                         </li>
+                        )}
                       </ol>
                     </form>
                   </div>
