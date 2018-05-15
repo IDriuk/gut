609e78dbd3ccbb0b2c79f2a7a4dcdfe6c05c9c73
Ivan Driuk <veytarn@gmail.com>
2018-05-15 09:28:19 +0300

    add years filter list

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -632,6 +632,16 @@ class Main extends Component {
 
                   </div>
 
+                  <div className="col-2 float-right pl-5">
+                    <ul className="filter-list small">
+                      {[8, 7, 6, 5, 4].map(i =>
+                        <li key={i} >
+                          <a className={`filter-item mb-2 px-3 py-2 ${i===8 ? "selected" : ""}`}>201{i}</a> 
+                        </li>
+                      )}
+                    </ul>
+                  </div>
+
                 </div>
               </div>
 
