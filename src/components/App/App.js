ff057d37bef54dc6241ceba388e27723fca98288
Ivan Driuk <veytarn@gmail.com>
2018-05-26 00:26:21 +0300

    add Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         </div>
 
         <Main />
+        <Footer />
       </div>
     );
   }
