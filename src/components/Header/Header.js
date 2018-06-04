7fd35f57d8c180be29e1f318d39cbad5dabfe521
Ivan Driuk <veytarn@gmail.com>
2018-06-04 18:03:31 +0300

    add dropdown open styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -64,7 +64,7 @@ class Header extends Component {
                 </li>
                 <li className="dropdown">
                   {/* trick: details on click automatically add open atribut */}
-                  <details className="d-flex details-reset dropdown-details flex-items-center px-2">
+                  <details className="details-expanded d-flex details-reset dropdown-details flex-items-center px-2">
                     <summary className="HeaderNavlink">
                       <svg className="octicon octicon-plus float-left mr-1 mt-1" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true">
                         <path fillRule="evenodd" d="M12 9H7v5H5V9H0V7h5V2h2v5h5z"></path>
