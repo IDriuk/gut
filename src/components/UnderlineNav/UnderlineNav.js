7d9e066018f170d26d6839e9beeb00b07c93433f
Ivan Driuk <veytarn@gmail.com>
2018-06-10 12:33:21 +0300

    add placeholder for stiky to underline nav

--- src/components/UnderlineNav/UnderlineNav.js
+++ src/components/UnderlineNav/UnderlineNav.js
@@ -1,33 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './UnderlineNav.css';
 
+// skey: sticky, sticky-js, jQuery Sticky Kit
 
 class UnderlineNav extends Component {
   render() {
     return (
-      <div className="UnderlineNav top-0 user-profile-nav">
-        <nav className="UnderlineNav-body">
-          {
-            [
-              ["Overview", null],
-              ["Repositories", 16],
-              ["Stars", 7],
-              ["Followers", 7],
-              ["Following", 2]]
-              .map(([title, counter], index) =>
-                index !== 0 ?
-                  <a key={index} className="UnderlineNav-item">
-                    {title}
-                    &nbsp;
-                    {counter && <span className="Counter">{counter}</span>}
-                  </a> :
-                  <a key={index} className={"UnderlineNav-item selected"}>
-                    {title}
-                  </a>
-              )
-          }
-        </nav>
-      </div>
+      <Fragment>
+        <div
+          className="UnderlineNav user-profile-nav top-0 is-placeholder"
+          style={visibility: "hidden", display: "none", height: 55}
+        ></div>
+        <div className="UnderlineNav user-profile-nav top-0">
+          <nav className="UnderlineNav-body">
+            {
+              [
+                ["Overview", null],
+                ["Repositories", 16],
+                ["Stars", 7],
+                ["Followers", 7],
+                ["Following", 2]]
+                .map(([title, counter], index) =>
+                  index !== 0 ?
+                    <a key={index} className="UnderlineNav-item">
+                      {title}
+                      &nbsp;
+                      {counter && <span className="Counter">{counter}</span>}
+                    </a> :
+                    <a key={index} className={"UnderlineNav-item selected"}>
+                      {title}
+                    </a>
+                )
+            }
+          </nav>
+        </div>
+      </Fragment>
     );
   }
 }
