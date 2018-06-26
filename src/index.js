9fdb7f2da1ed650944220aab68a76cc625b3e0d1
Ivan Driuk <veytarn@gmail.com>
2018-06-26 12:13:57 +0300

    wrap app with redux provider, add redux devtools

--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+
+const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+ReactDOM.render(
+  <Provider store={createStore(reducers, reduxDevtools )}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
