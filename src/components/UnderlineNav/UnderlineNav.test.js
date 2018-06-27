0d9318ce8bc6f35ae0b731570bb980b41bff0449
Ivan Driuk <veytarn@gmail.com>
2018-06-27 10:12:04 +0300

    fix UnderlineNav test

--- src/components/UnderlineNav/UnderlineNav.test.js
+++ src/components/UnderlineNav/UnderlineNav.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UnderlineNav from './UnderlineNav';
+import { navCategoriesCounts } from '../../actions/data';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<UnderlineNav />, div);
+  ReactDOM.render(
+    <UnderlineNav
+      fetchNavCategoriesCounts={() => navCategoriesCounts}
+      navCategoriesCounts={navCategoriesCounts}
+    />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
